af2080ec242c4a9a51739bf088b39d2827e94042
tuantrankim <tuantrankim@gmail.com>
2020-04-26 14:05:01 -0700

    Save Gig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -12,7 +14,7 @@ namespace GigHub.Controllers
         {
             _context = new ApplicationDbContext();
         }
-
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -20,6 +22,27 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            //var artist = _context.Users.Single(u => u.Id == artistId);
+            //var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                ArtistId = artistId,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+            
         }
     }
 }
